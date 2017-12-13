0d53c1394077ecf432a1e7a1c5c0f162d01bc3c2
GaneshSPatil <ganeshpl@thoughtworks.com>
2017-12-13 13:00:25 +0530

    Adds de-serialization model for JobIdentifier. The JobIdentifier is passed along as a parameter to the createAgent request for elastic agent plugins

---
+++ src/main/java/cd/go/contrib/elasticagent/model/JobIdentifier.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2017 ThoughtWorks, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cd.go.contrib.elasticagent.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class JobIdentifier {
+    @Expose
+    @SerializedName("pipeline_name")
+    private String pipelineName;
+
+    @Expose
+    @SerializedName("pipeline_counter")
+    private final Long pipelineCounter;
+
+    @Expose
+    @SerializedName("pipeline_label")
+    private final String pipelineLabel;
+
+    @Expose
+    @SerializedName("stage_name")
+    private final String staqeName;
+
+    @Expose
+    @SerializedName("stage_counter")
+    private final String stageCounter;
+
+    @Expose
+    @SerializedName("job_name")
+    private final String jobName;
+
+    @Expose
+    @SerializedName("job_id")
+    private final Long jobId;
+
+    public JobIdentifier() {
+        pipelineCounter = null;
+        pipelineLabel = null;
+        staqeName = null;
+        stageCounter = null;
+        jobName = null;
+        jobId = null;
+    }
+
+    public JobIdentifier(String pipelineName, long pipelineCounter, String pipelineLabel, String staqeName, String stageCounter, String jobName, long jobId) {
+        this.pipelineName = pipelineName;
+        this.pipelineCounter = pipelineCounter;
+        this.pipelineLabel = pipelineLabel;
+        this.staqeName = staqeName;
+        this.stageCounter = stageCounter;
+        this.jobName = jobName;
+        this.jobId = jobId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof JobIdentifier)) return false;
+
+        JobIdentifier that = (JobIdentifier) o;
+
+        if (pipelineCounter != that.pipelineCounter) return false;
+        if (jobId != that.jobId) return false;
+        if (pipelineName != null ? !pipelineName.equals(that.pipelineName) : that.pipelineName != null) return false;
+        if (pipelineLabel != null ? !pipelineLabel.equals(that.pipelineLabel) : that.pipelineLabel != null)
+            return false;
+        if (staqeName != null ? !staqeName.equals(that.staqeName) : that.staqeName != null) return false;
+        if (stageCounter != null ? !stageCounter.equals(that.stageCounter) : that.stageCounter != null) return false;
+        return jobName != null ? jobName.equals(that.jobName) : that.jobName == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = pipelineName != null ? pipelineName.hashCode() : 0;
+        result = 31 * result + (int) (pipelineCounter ^ (pipelineCounter >>> 32));
+        result = 31 * result + (pipelineLabel != null ? pipelineLabel.hashCode() : 0);
+        result = 31 * result + (staqeName != null ? staqeName.hashCode() : 0);
+        result = 31 * result + (stageCounter != null ? stageCounter.hashCode() : 0);
+        result = 31 * result + (jobName != null ? jobName.hashCode() : 0);
+        result = 31 * result + (int) (jobId ^ (jobId >>> 32));
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "JobIdentifier{" +
+                "pipelineName='" + pipelineName + '\'' +
+                ", pipelineCounter=" + pipelineCounter +
+                ", pipelineLabel='" + pipelineLabel + '\'' +
+                ", staqeName='" + staqeName + '\'' +
+                ", stageCounter='" + stageCounter + '\'' +
+                ", jobName='" + jobName + '\'' +
+                ", jobId=" + jobId +
+                '}';
+    }
+}
