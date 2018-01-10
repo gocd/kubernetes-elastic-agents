e7d9488bffaf7bb4a28ad81e70644138dd3e4e44
GaneshSPatil <ganeshpl@thoughtworks.com>
2018-01-10 14:33:36 +0530

    Add Kubernetes Pod logs details

--- src/main/java/cd/go/contrib/elasticagent/model/reports/agent/KubernetesElasticAgent.java
+++ src/main/java/cd/go/contrib/elasticagent/model/reports/agent/KubernetesElasticAgent.java
@@ -1,5 +1,6 @@
 package cd.go.contrib.elasticagent.model.reports.agent;
 
+import cd.go.contrib.elasticagent.Constants;
 import cd.go.contrib.elasticagent.model.JobIdentifier;
 import io.fabric8.kubernetes.api.model.Event;
 import io.fabric8.kubernetes.api.model.Pod;
@@ -12,6 +13,7 @@ public class KubernetesElasticAgent {
     private GoCDContainerDetails agentDetails;
     private String elasticAgentId;
     private ArrayList<KubernetesPodEvent> events;
+    private String logs;
 
     public static KubernetesElasticAgent fromPod(KubernetesClient client, Pod pod, String elasticAgentId, JobIdentifier jobIdentifier) {
         KubernetesElasticAgent agent = new KubernetesElasticAgent();
@@ -19,7 +21,24 @@ public class KubernetesElasticAgent {
         agent.podDetails = KubernetesPodDetails.fromPod(pod);
         agent.agentDetails = GoCDContainerDetails.fromContainer(pod.getSpec().getContainers().get(0), pod.getStatus().getContainerStatuses().get(0));
         agent.events = getAllEventsForPod(pod, client);
+        agent.logs = getPodLogs(pod, client);
         return agent;
+    }
+
+    public ArrayList<KubernetesPodEvent> getEvents() {
+        return events;
+    }
+
+    public KubernetesPodDetails getPodDetails() {
+        return podDetails;
+    }
+
+    public GoCDContainerDetails getAgentDetails() {
+        return agentDetails;
+    }
+
+    public String getElasticAgentId() {
+        return elasticAgentId;
     }
 
     private static ArrayList<KubernetesPodEvent> getAllEventsForPod(Pod pod, KubernetesClient client) {
@@ -42,19 +61,9 @@ public class KubernetesElasticAgent {
         return events;
     }
 
-    public ArrayList<KubernetesPodEvent> getEvents() {
-        return events;
+    private static String getPodLogs(Pod pod, KubernetesClient client) {
+        return client.pods().inNamespace(Constants.KUBERNETES_NAMESPACE)
+                .withName(pod.getMetadata().getName()).getLog();
     }
 
-    public KubernetesPodDetails getPodDetails() {
-        return podDetails;
-    }
-
-    public GoCDContainerDetails getAgentDetails() {
-        return agentDetails;
-    }
-
-    public String getElasticAgentId() {
-        return elasticAgentId;
-    }
 }
