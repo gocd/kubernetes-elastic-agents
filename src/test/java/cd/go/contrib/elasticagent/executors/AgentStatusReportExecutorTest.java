7fd5bb30e014b2fcf2e9e639ed08d43c468096fa
GaneshSPatil <ganeshpl@thoughtworks.com>
2018-01-11 17:24:02 +0530

    Add test to verify the get status report using elastic agent id behavior

---
+++ src/test/java/cd/go/contrib/elasticagent/executors/AgentStatusReportExecutorTest.java
@@ -0,0 +1,123 @@
+package cd.go.contrib.elasticagent.executors;
+
+import cd.go.contrib.elasticagent.Constants;
+import cd.go.contrib.elasticagent.KubernetesClientFactory;
+import cd.go.contrib.elasticagent.PluginRequest;
+import cd.go.contrib.elasticagent.PluginSettings;
+import cd.go.contrib.elasticagent.builders.PluginStatusReportViewBuilder;
+import cd.go.contrib.elasticagent.requests.AgentStatusReportRequest;
+import com.thoughtworks.go.plugin.api.response.GoPluginApiResponse;
+import io.fabric8.kubernetes.api.model.*;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.dsl.MixedOperation;
+import io.fabric8.kubernetes.client.dsl.NonNamespaceOperation;
+import io.fabric8.kubernetes.client.dsl.PodResource;
+import io.fabric8.kubernetes.client.dsl.Resource;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+public class AgentStatusReportExecutorTest {
+
+    private Pod pod;
+    private AgentStatusReportExecutor executor;
+    private String elasticAgentId = "elastic-agent-id";
+
+    @Mock
+    private AgentStatusReportRequest statusReportRequest;
+
+    @Mock
+    private PluginRequest pluginRequest;
+
+    @Mock
+    private KubernetesClientFactory kubernetesClientFactory;
+
+    @Mock
+    private KubernetesClient client;
+
+    @Mock
+    private PluginStatusReportViewBuilder builder;
+
+    @Mock
+    private MixedOperation<Pod, PodList, DoneablePod, PodResource<Pod, DoneablePod>> mockedOperation;
+
+    @Mock
+    private NonNamespaceOperation<Pod, PodList, DoneablePod, PodResource<Pod, DoneablePod>> mockedNamespaceOperation;
+
+    @Mock
+    private PodList podList;
+
+    @Mock
+    private MixedOperation<Event, EventList, DoneableEvent, Resource<Event, DoneableEvent>> events;
+
+    @Mock
+    private NonNamespaceOperation<Event, EventList, DoneableEvent, Resource<Event, DoneableEvent>> eventspace;
+
+    @Mock
+    private EventList eventsList;
+
+    @Mock
+    private PodResource<Pod, DoneablePod> podresource;
+
+    @Before
+    public void setUp() throws Exception {
+        initMocks(this);
+        pod = creatdDefaultPod();
+        pod.getMetadata().setName(elasticAgentId);
+        executor = new AgentStatusReportExecutor(statusReportRequest, pluginRequest, kubernetesClientFactory, builder);
+
+        when(client.pods()).thenReturn(mockedOperation);
+        when(mockedOperation.inNamespace(Constants.KUBERNETES_NAMESPACE)).thenReturn(mockedNamespaceOperation);
+        when(mockedNamespaceOperation.list()).thenReturn(podList);
+        when(podList.getItems()).thenReturn(Arrays.asList(pod));
+
+        when(mockedNamespaceOperation.withName(elasticAgentId)).thenReturn(podresource);
+        when(podresource.getLog()).thenReturn("agent-logs");
+
+        when(client.events()).thenReturn(events);
+        when(events.inAnyNamespace()).thenReturn(eventspace);
+        when(eventspace.list()).thenReturn(eventsList);
+        when(eventsList.getItems()).thenReturn(new ArrayList<>());
+    }
+
+    @Test
+    public void shouldReturnAgentStatusReportBasedOnProvidedElasticAgentId() throws Exception {
+
+        when(statusReportRequest.getJobIdentifier()).thenReturn(null);
+        when(statusReportRequest.getElasticAgentId()).thenReturn(elasticAgentId);
+
+        PluginSettings pluginSettings = new PluginSettings();
+
+        when(pluginRequest.getPluginSettings()).thenReturn(pluginSettings);
+        when(kubernetesClientFactory.kubernetes(pluginSettings)).thenReturn(client);
+
+        when(builder.build(any(), any())).thenReturn("my-view");
+
+        GoPluginApiResponse response = executor.execute();
+
+        assertThat(response.responseCode(), is(200));
+        assertThat(response.responseBody(), is("{\"view\":\"my-view\"}"));
+    }
+
+    private Pod creatdDefaultPod() {
+        Pod pod = new Pod();
+        pod.setMetadata(new ObjectMeta());
+        PodSpec spec = new PodSpec();
+        spec.setContainers(Arrays.asList(new Container()));
+        pod.setSpec(spec);
+        PodStatus status = new PodStatus();
+        status.setContainerStatuses(Arrays.asList(new ContainerStatus()));
+        pod.setStatus(status);
+        return pod;
+    }
+
+}
