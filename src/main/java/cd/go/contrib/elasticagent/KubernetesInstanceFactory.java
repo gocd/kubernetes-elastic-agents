c68aab64b6a3cdf433a8dc284fde221a8b929865
Bhupendrakumar Piprava <bdpiparva@gmail.com>
2018-02-06 14:28:43 +0530

    Run container in privileged mode to support docker-in-docker and docker-out-of-docker.

--- src/main/java/cd/go/contrib/elasticagent/KubernetesInstanceFactory.java
+++ src/main/java/cd/go/contrib/elasticagent/KubernetesInstanceFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 ThoughtWorks, Inc.
+ * Copyright 2018 ThoughtWorks, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -57,6 +57,7 @@ public class KubernetesInstanceFactory {
         container.setName(containerName);
         container.setImage(image(request.properties()));
         container.setImagePullPolicy("IfNotPresent");
+        container.setSecurityContext(new SecurityContextBuilder().withPrivileged(true).build());
 
         container.setResources(getPodResources(request));
 
