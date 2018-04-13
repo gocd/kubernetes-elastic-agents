316eb28a766838e5bdf6dad149d84a6b4c8d2142
Varsha Varadarajan <varshasvaradarajan@gmail.com>
2018-04-13 15:58:19 +0530

    Handle NumberFormatException when desrializing agent autoregister count and maximum pending pods.

--- src/main/java/cd/go/contrib/elasticagent/utils/Util.java
+++ src/main/java/cd/go/contrib/elasticagent/utils/Util.java
@@ -18,6 +18,11 @@ package cd.go.contrib.elasticagent.utils;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSyntaxException;
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import com.google.gson.stream.JsonWriter;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -92,4 +97,30 @@ public class Util {
         int digitGroups = (int) (Math.log10(size) / Math.log10(1024));
         return new DecimalFormat("#,##0.##").format(size / Math.pow(1024, digitGroups)) + " " + units[digitGroups];
     }
+
+    public static TypeAdapter<Number> IntTypeAdapter = new TypeAdapter<Number>() {
+
+        @Override
+        public void write(JsonWriter out, Number value)
+                throws IOException {
+            out.value(value);
+        }
+
+        @Override
+        public Number read(JsonReader in) throws IOException {
+            if (in.peek() == JsonToken.NULL) {
+                in.nextNull();
+                return null;
+            }
+            try {
+                String result = in.nextString();
+                if ("".equals(result)) {
+                    return null;
+                }
+                return Integer.parseInt(result);
+            } catch (NumberFormatException e) {
+                throw new JsonSyntaxException(e);
+            }
+        }
+    };
 }
